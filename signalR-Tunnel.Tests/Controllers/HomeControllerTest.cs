f88bc75b0418b9dc9bb9a2a3b1087cd0655e1342
festone000 <festone000@hotmail.com>
2016-08-25 14:05:36 +0800

    1

---
+++ signalR-Tunnel.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using signalR_Tunnel.Controllers;
+
+namespace signalR_Tunnel.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+      
+    }
+}
