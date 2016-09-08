9d57ffa0ad78790857123987bbb08e0d35fa4108
festone000 <festone000@hotmail.com>
2016-09-08 22:26:40 +0800

    试图加入百度UMEditor

--- signalR-Tunnel/Controllers/HomeController.cs
+++ signalR-Tunnel/Controllers/HomeController.cs
@@ -4,28 +4,28 @@ namespace signalR_Tunnel.Controllers
 {
     public class HomeController : Controller
     {
-        public bool CheckPassword(string password) {
-            return password == Util.Util.pswd;
-        }
+        //public bool CheckPassword(string password) {
+        //    return password == Util.Util.pswd;
+        //}
 
         public ActionResult Index(string password)
         {
 
-            if (password != Util.Util.pswd) {
-                RedirectToAction("PasswordError", "Error");
-            }
+            //if (password != Util.Util.pswd) {
+            //    RedirectToAction("PasswordError", "Error");
+            //}
 
             return View();
         }
 
-        public ActionResult FileSubmit() {
+        //public ActionResult FileSubmit() {
 
-            var file = Request.Files["file"];
+        //    var file = Request.Files["file"];
 
 
-            return View();
+        //    return View();
 
-        }
+        //}
 
     }
 }
