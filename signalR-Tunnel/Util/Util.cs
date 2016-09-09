65011efea2ff6b34b6f8e695cb6f5fa4d57912a9
festone000 <festone000@hotmail.com>
2016-09-09 08:27:37 +0800

    1

---
+++ signalR-Tunnel/Util/Util.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace signalR_Tunnel.Util
+{
+    public class Util
+    {
+        public static string pswd = ConfigurationManager.AppSettings["loginPswd"].ToString();
+    }
+}
