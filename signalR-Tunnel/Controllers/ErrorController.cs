946e33bd887ff245739e25f96e3a377783b224c8
festone000 <festone000@hotmail.com>
2016-09-13 10:30:05 +0800

    1

---
+++ signalR-Tunnel/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace signalR_Tunnel.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+        public ActionResult PasswordError()
+        {
+            ViewBag.Title = "密码错误，你想干啥?";
+            return View();
+        }
+	}
+}
